a6327b2836c876b781fdba0a6ffe9cda671a6d04
Devdan <dannorvan@gmail.com>
2021-11-28 15:22:34 -0600

    nested routes ready

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -11,26 +11,6 @@ const Navigation = () => {
                 <div className="main-layout">
                     <nav>
                         <img src={logo} alt="logooo"/>
-                        {/*<ul>*/}
-                        {/*    <li>*/}
-                        {/*        <NavLink*/}
-                        {/*            to="/lazy1"*/}
-                        {/*            className={({isActive}) => isActive ? 'nav-active' : ''}*/}
-                        {/*        >Lazy 1</NavLink>*/}
-                        {/*    </li>*/}
-                        {/*    <li>*/}
-                        {/*        <NavLink*/}
-                        {/*            to="/lazy2"*/}
-                        {/*            className={({isActive}) => isActive ? 'nav-active' : ''}*/}
-                        {/*        >Lazy 2</NavLink>*/}
-                        {/*    </li>*/}
-                        {/*    <li>*/}
-                        {/*        <NavLink*/}
-                        {/*            to="/lazy3"*/}
-                        {/*            className={({isActive}) => isActive ? 'nav-active' : ''}*/}
-                        {/*        >Lazy 3</NavLink>*/}
-                        {/*    </li>*/}
-                        {/*</ul>*/}
                         <ul>
                             {
                                 routes.map(({to, name}) => (
@@ -44,13 +24,6 @@ const Navigation = () => {
                             }
                         </ul>
                     </nav>
-                    {/*<Routes>*/}
-                    {/*    <Route path="lazy1" element={<LazyPage1/>}/>*/}
-                    {/*    <Route path="lazy2" element={<LazyPage2/>}/>*/}
-                    {/*    <Route path="lazy3" element={<LazyPage3/>}/>*/}
-
-                    {/*    <Route path="*" element={<Navigate to="/lazy1" replace={true}/>}/>*/}
-                    {/*</Routes>*/}
 
                     <Routes>
                         {
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -13,8 +13,7 @@ interface Route {
 }
 
 const LazyLayout = lazy(() => import(/*webpackChunkName: "LazyLayout"*/ '../01-lazyload/layout/LazyLayout'));
-// const Lazy2 = lazy(() => import(/*webpackChunkName: "LazyPage2"*/ '../01-lazyload/pages/LazyPage2'));
-// const Lazy3 = lazy(() => import(/*webpackChunkName: "LazyPage3"*/ '../01-lazyload/pages/LazyPage3'));
+
 
 export const routes: Route[] = [
     {
@@ -29,17 +28,4 @@ export const routes: Route[] = [
         Component: NoLazy,
         name: 'No Lazy'
     },
-    // {
-    //     to: '/lazy2',
-    //     path: 'lazy2',
-    //     Component: Lazy2,
-    //     name: 'Lazy-2'
-    // },
-    // {
-    //     to: '/lazy3',
-    //     path: 'lazy3',
-    //     Component: Lazy3,
-    //     name: 'Lazy-3'
-    // },
-
 ]
